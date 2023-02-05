12aa1bb6877f1809bd6511f977a9801144b52cf3
ilyazverev <ilya.zverev@umbrellait.com>
2023-02-05 11:12:25 +0300

    add new structure by Feature-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React, {Suspense, useContext} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import './styles/index.scss'
-import {Theme, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
-
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
 const {theme, toggleTheme} = useTheme()
@@ -16,8 +14,8 @@ const {theme, toggleTheme} = useTheme()
             <Link to={'/about'}>Обо мне</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
             <Routes>
-                <Route path={'/about'} element={<AboutPageAsync/>}/>
-                <Route path={'/'} element={<MainPageAsync/>}/>
+                <Route path={'/about'} element={<AboutPage/>}/>
+                <Route path={'/'} element={<MainPage/>}/>
             </Routes>
             </Suspense>
             <button onClick={toggleTheme}>Жми меня и меняй тему</button>
